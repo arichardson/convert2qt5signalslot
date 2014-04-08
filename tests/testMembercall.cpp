b5baa6d37f2422393ded74ea1c467140f4119143
Alex Richardson <arichardson.kde@gmail.com>
2014-04-08 11:01:18 +0200

    Add test case for issue #9

--- tests/testMembercall.cpp
+++ tests/testMembercall.cpp
@@ -13,9 +13,13 @@ private:
 };
 
 Test::Test() {
-    //here connect receiver/disconnect sender is "this"
+    // inside QObject subclass -> don't have to prefix with QObject::
+
+    // here connect receiver/disconnect sender is "this"
     connect(q, SIGNAL(objectNameChanged(QString)), SLOT(deleteLater()));
     disconnect(SIGNAL(objectNameChanged(QString)), q, SLOT(deleteLater()));
+    this->connect(q, SIGNAL(objectNameChanged(QString)), SLOT(deleteLater()));
+    this->disconnect(SIGNAL(objectNameChanged(QString)), q, SLOT(deleteLater()));
     //ensure that the connect receiver/disconnect sender is correctly set ("q")
     q->connect(this, SIGNAL(objectNameChanged(QString)), SLOT(deleteLater()));
     q->disconnect(SIGNAL(objectNameChanged(QString)), this, SLOT(deleteLater()));
@@ -43,6 +47,16 @@ Test::Test() {
     (*q).connect(this, SIGNAL(objectNameChanged(QString)), SLOT(deleteLater()));
     (*q).disconnect(SIGNAL(objectNameChanged(QString)), this, SLOT(deleteLater()));
 }
+
+void nonMember() {
+    Test* test = nullptr;
+    QObject* obj = nullptr;
+    // now we need the QObject:: added
+    test->connect(obj, SIGNAL(objectNameChanged(QString)), SLOT(deleteLater()));
+    obj->connect(test, SIGNAL(objectNameChanged(QString)), SLOT(deleteLater()));
+    test->disconnect(SIGNAL(objectNameChanged(QString)), obj, SLOT(deleteLater()));
+    obj->disconnect(SIGNAL(objectNameChanged(QString)), test, SLOT(deleteLater()));
+}
 )delim";
 
 static const char* output = R"delim(
@@ -58,39 +72,53 @@ private:
 };
 
 Test::Test() {
-    //here connect receiver/disconnect sender is "this"
+    // inside QObject subclass -> don't have to prefix with QObject::
+
+    // here connect receiver/disconnect sender is "this"
+    connect(q, &QObject::objectNameChanged, this, &Test::deleteLater);
+    disconnect(this, &Test::objectNameChanged, q, &QObject::deleteLater);
     connect(q, &QObject::objectNameChanged, this, &Test::deleteLater);
     disconnect(this, &Test::objectNameChanged, q, &QObject::deleteLater);
     //ensure that the connect receiver/disconnect sender is correctly set ("q")
-    q->connect(this, &Test::objectNameChanged, q, &QObject::deleteLater);
-    q->disconnect(q, &QObject::objectNameChanged, this, &Test::deleteLater);
+    connect(this, &Test::objectNameChanged, q, &QObject::deleteLater);
+    disconnect(q, &QObject::objectNameChanged, this, &Test::deleteLater);
     //now try with a function call
-    someObj()->connect(this, &Test::objectNameChanged, someObj(), &QObject::deleteLater);
-    someObj()->disconnect(someObj(), &QObject::objectNameChanged, this, &Test::deleteLater);
+    connect(this, &Test::objectNameChanged, someObj(), &QObject::deleteLater);
+    disconnect(someObj(), &QObject::objectNameChanged, this, &Test::deleteLater);
 
     QObject value;
-    value.connect(this, &Test::objectNameChanged, &value, &QObject::deleteLater);
-    value.disconnect(&value, &QObject::objectNameChanged, this, &Test::deleteLater);
+    connect(this, &Test::objectNameChanged, &value, &QObject::deleteLater);
+    disconnect(&value, &QObject::objectNameChanged, this, &Test::deleteLater);
 
     QObject& ref = *q;
-    ref.connect(this, &Test::objectNameChanged, &ref, &QObject::deleteLater);
-    ref.disconnect(&ref, &QObject::objectNameChanged, this, &Test::deleteLater);
+    connect(this, &Test::objectNameChanged, &ref, &QObject::deleteLater);
+    disconnect(&ref, &QObject::objectNameChanged, this, &Test::deleteLater);
 
     QObject*& ptrRef = q;
-    ptrRef->connect(this, &Test::objectNameChanged, ptrRef, &QObject::deleteLater);
-    ptrRef->disconnect(ptrRef, &QObject::objectNameChanged, this, &Test::deleteLater);
+    connect(this, &Test::objectNameChanged, ptrRef, &QObject::deleteLater);
+    disconnect(ptrRef, &QObject::objectNameChanged, this, &Test::deleteLater);
 
     QObject** ptrPtr = &q;
-    (*ptrPtr)->connect(this, &Test::objectNameChanged, (*ptrPtr), &QObject::deleteLater);
-    (*ptrPtr)->disconnect((*ptrPtr), &QObject::objectNameChanged, this, &Test::deleteLater);
+    connect(this, &Test::objectNameChanged, (*ptrPtr), &QObject::deleteLater);
+    disconnect((*ptrPtr), &QObject::objectNameChanged, this, &Test::deleteLater);
 
     // dereferenced pointer
-    (*q).connect(this, &Test::objectNameChanged, &(*q), &QObject::deleteLater);
-    (*q).disconnect(&(*q), &QObject::objectNameChanged, this, &Test::deleteLater);
+    connect(this, &Test::objectNameChanged, &(*q), &QObject::deleteLater);
+    disconnect(&(*q), &QObject::objectNameChanged, this, &Test::deleteLater);
+}
+
+void nonMember() {
+    Test* test = nullptr;
+    QObject* obj = nullptr;
+    // now we need the QObject:: added
+    QObject::connect(obj, &QObject::objectNameChanged, test, &Test::deleteLater);
+    QObject::connect(test, &Test::objectNameChanged, obj, &QObject::deleteLater);
+    QObject::disconnect(test, &Test::objectNameChanged, obj, &QObject::deleteLater);
+    QObject::disconnect(obj, &QObject::objectNameChanged, test, &Test::deleteLater);
 }
 )delim";
 
 int main() {
-    return testMain(input, output, 16, 16);
+    return testMain(input, output, 22, 22);
 }
 
