592d77f5f429ad96e068b12f492a24b94be9f117
Alex Richardson <arichardson.kde@gmail.com>
2014-04-11 16:25:13 +0200

    Use FindLLVM and FindClang from kdev-clang

--- cmake/FindLLVM.cmake
+++ cmake/FindLLVM.cmake
@@ -1,63 +1,109 @@
-# Detect LLVM and set various variable to link against the different component of LLVM
+# Find the native LLVM includes and libraries
 #
-# NOTE: This is a modified version of the module originally found in the OpenGTL project
-# at www.opengtl.org
+# Defines the following variables
+#  LLVM_INCLUDE_DIR - where to find llvm include files
+#  LLVM_LIBRARY_DIR - where to find llvm libs
+#  LLVM_CFLAGS      - llvm compiler flags
+#  LLVM_LFLAGS      - llvm linker flags
+#  LLVM_MODULE_LIBS - list of llvm libs for working with modules.
+#  LLVM_FOUND       - True if llvm found.
+#  LLVM_VERSION     - Version string ("llvm-config --version")
 #
-# LLVM_BIN_DIR : directory with LLVM binaries
-# LLVM_LIB_DIR : directory with LLVM library
-# LLVM_INCLUDE_DIR : directory with LLVM include
+# This module reads hints about search locations from variables
+#  LLVM_ROOT        - Preferred LLVM installation prefix (containing bin/, lib/, ...)
 #
-# LLVM_COMPILE_FLAGS : compile flags needed to build a program using LLVM headers
-# LLVM_LDFLAGS : ldflags needed to link
-# LLVM_LIBS_CORE : ldflags needed to link against a LLVM core library
-# LLVM_LIBS_JIT : ldflags needed to link against a LLVM JIT
-# LLVM_LIBS_JIT_OBJECTS : objects you need to add to your source when using LLVM JIT
+#  Note: One may specify these as environment variables if they are not specified as
+#   CMake variables or cache entries.
 
-if (LLVM_INCLUDE_DIR)
-  set(LLVM_FOUND TRUE)
-else (LLVM_INCLUDE_DIR)
+#=============================================================================
+# Copyright 2014 Kevin Funk <kfunk@kde.org>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
 
-find_program(LLVM_CONFIG_EXECUTABLE
-  NAMES llvm-config
-  PATHS
-  /opt/local/bin
-)
+if (NOT LLVM_ROOT AND DEFINED ENV{LLVM_ROOT})
+    file(TO_CMAKE_PATH "$ENV{LLVM_ROOT}" LLVM_ROOT)
+endif()
 
-MACRO(FIND_LLVM_LIBS LLVM_CONFIG_EXECUTABLE _libname_ LIB_VAR OBJECT_VAR)
-  exec_program( ${LLVM_CONFIG_EXECUTABLE} ARGS --libs ${_libname_}  OUTPUT_VARIABLE ${LIB_VAR} )
-  STRING(REGEX MATCHALL "[^ ]*[.]o[ $]"  ${OBJECT_VAR} ${${LIB_VAR}})
-  SEPARATE_ARGUMENTS(${OBJECT_VAR})
-  STRING(REGEX REPLACE "[^ ]*[.]o[ $]" ""  ${LIB_VAR} ${${LIB_VAR}})
-ENDMACRO(FIND_LLVM_LIBS)
+# if the user specified LLVM_ROOT, use that and fail otherwise
+if (LLVM_ROOT)
+  find_program(LLVM_CONFIG_EXECUTABLE NAMES llvm-config HINTS ${LLVM_ROOT}/bin DOC "llvm-config executable" NO_DEFAULT_PATH)
+else()
+  # find llvm-config, prefer the one with a version suffix, e.g. llvm-config-3.3
+  # note: on some distributions, only 'llvm-config' is shipped, so let's always try to fallback on that
+  find_program(LLVM_CONFIG_EXECUTABLE NAMES llvm-config-${LLVM_FIND_VERSION} llvm-config DOC "llvm-config executable")
 
+  # other distributions don't ship llvm-config, but only some llvm-config-VERSION binary
+  # try to deduce installed LLVM version by looking up llvm-nm in PATH and *then* find llvm-config-VERSION via that
+  if (NOT LLVM_CONFIG_EXECUTABLE)
+    find_program(_llvmNmExecutable llvm-nm)
+    if (_llvmNmExecutable)
+      execute_process(COMMAND ${_llvmNmExecutable} --version OUTPUT_VARIABLE _out)
+      string(REGEX REPLACE ".*LLVM version ([^ \n]+).*" "\\1" _versionString "${_out}")
+      find_program(LLVM_CONFIG_EXECUTABLE NAMES llvm-config-${_versionString} DOC "llvm-config executable")
+    endif()
+  endif()
+endif()
 
-exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --bindir OUTPUT_VARIABLE LLVM_BIN_DIR )
-exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --libdir OUTPUT_VARIABLE LLVM_LIB_DIR )
-#MESSAGE(STATUS "LLVM lib dir: " ${LLVM_LIB_DIR})
-exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --includedir OUTPUT_VARIABLE LLVM_INCLUDE_DIR )
+set(LLVM_FOUND FALSE)
 
+if (LLVM_CONFIG_EXECUTABLE)
+  # verify that we've found the correct version of llvm-config
+  execute_process(COMMAND ${LLVM_CONFIG_EXECUTABLE} --version
+    OUTPUT_VARIABLE LLVM_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --cxxflags  OUTPUT_VARIABLE LLVM_COMPILE_FLAGS )
-MESSAGE(STATUS "LLVM CXX flags: " ${LLVM_COMPILE_FLAGS})
-exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --ldflags   OUTPUT_VARIABLE LLVM_LDFLAGS )
-MESSAGE(STATUS "LLVM LD flags: " ${LLVM_LDFLAGS})
-exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --libs      OUTPUT_VARIABLE LLVM_LIBS_CORE )
-MESSAGE(STATUS "LLVM core libs: " ${LLVM_LIBS_CORE})
-FIND_LLVM_LIBS( ${LLVM_CONFIG_EXECUTABLE} "jit native" LLVM_LIBS_JIT LLVM_LIBS_JIT_OBJECTS )
-#STRING(REPLACE " -lLLVMCore -lLLVMSupport -lLLVMSystem" "" LLVM_LIBS_JIT ${LLVM_LIBS_JIT_RAW})
-MESSAGE(STATUS "LLVM JIT libs: " ${LLVM_LIBS_JIT})
-MESSAGE(STATUS "LLVM JIT objs: " ${LLVM_LIBS_JIT_OBJECTS})
+  if (NOT LLVM_VERSION)
+    set(_LLVM_ERROR_MESSAGE "Failed to parse version from llvm-config")
+  elseif (LLVM_FIND_VERSION VERSION_GREATER LLVM_VERSION)
+    set(_LLVM_ERROR_MESSAGE "LLVM version too old: ${LLVM_VERSION}")
+  else()
+    message(STATUS "Found LLVM (version: ${LLVM_VERSION}): (using ${LLVM_CONFIG_EXECUTABLE})")
+    set(LLVM_FOUND TRUE)
+  endif()
+else()
+  set(_LLVM_ERROR_MESSAGE "Could NOT find 'llvm-config' executable")
+endif()
 
-if(LLVM_INCLUDE_DIR)
-  set(LLVM_FOUND TRUE)
-endif(LLVM_INCLUDE_DIR)
+if (LLVM_FOUND)
+  execute_process(
+    COMMAND ${LLVM_CONFIG_EXECUTABLE} --includedir
+    OUTPUT_VARIABLE LLVM_INCLUDE_DIR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
 
-if(LLVM_FOUND)
-  message(STATUS "Found LLVM: ${LLVM_INCLUDE_DIR}")
-else(LLVM_FOUND)
-  if(LLVM_FIND_REQUIRED)
-    message(FATAL_ERROR "Could NOT find LLVM")
-  endif(LLVM_FIND_REQUIRED)
-endif(LLVM_FOUND)
+  execute_process(
+    COMMAND ${LLVM_CONFIG_EXECUTABLE} --libdir
+    OUTPUT_VARIABLE LLVM_LIBRARY_DIR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
 
-endif (LLVM_INCLUDE_DIR)
+  execute_process(
+    COMMAND ${LLVM_CONFIG_EXECUTABLE} --cppflags
+    OUTPUT_VARIABLE LLVM_CFLAGS
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  execute_process(
+    COMMAND ${LLVM_CONFIG_EXECUTABLE} --ldflags
+    OUTPUT_VARIABLE LLVM_LFLAGS
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  execute_process(
+    COMMAND ${LLVM_CONFIG_EXECUTABLE} --libs core bitreader asmparser analysis
+    OUTPUT_VARIABLE LLVM_MODULE_LIBS
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+endif()
+
+if (LLVM_FIND_REQUIRED AND NOT LLVM_FOUND)
+  message(FATAL_ERROR "Could not find LLVM: ${_LLVM_ERROR_MESSAGE}")
+elseif(_LLVM_ERROR_MESSAGE)
+  message(STATUS "Could not find LLVM: ${_LLVM_ERROR_MESSAGE}")
+endif()
