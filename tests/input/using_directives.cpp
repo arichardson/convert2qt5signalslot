4cf829d7b5649219cc18ac4c104e10c2f85ab0f1
Alex Richardson <arichardson.kde@gmail.com>
2014-04-14 23:08:27 +0200

    update test case for #17 with more cases

--- tests/input/using_directives.cpp
+++ tests/input/using_directives.cpp
@@ -6,61 +6,161 @@ class Foo : public QObject {
 public:
     Foo() = default;
 Q_SIGNALS:
-    void sig1();
-    void sig2();
-    void sig2(int);
+    void sig();
+    void sig(int);
 };
+
+namespace Nested {
+class Bar : public QObject {
+    Q_OBJECT
+public:
+    Bar() = default;
+Q_SIGNALS:
+    void sig();
+    void sig(int);
+};
+}
 }
 
 static Namespace::Foo* foo = new Namespace::Foo();
+static Namespace::Nested::Bar* bar = new Namespace::Nested::Bar();
 
 namespace UsingNamespace {
     using namespace Namespace;
     void test() {
-        QObject::connect(foo, SIGNAL(sig1()), foo, SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig1()), SLOT(deleteLater()));
-        QObject::connect(foo, SIGNAL(sig2()), foo, SLOT(deleteLater()));
-        QObject::connect(foo, SIGNAL(sig2(int)), foo, SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig2()), SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig2(int)), SLOT(deleteLater()));
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
     }
 }
-
 namespace UsingNamespaceAfterFunc {
     // using directive is after function -> have to qualify
     void test() {
-        QObject::connect(foo, SIGNAL(sig1()), foo, SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig1()), SLOT(deleteLater()));
-        QObject::connect(foo, SIGNAL(sig2()), foo, SLOT(deleteLater()));
-        QObject::connect(foo, SIGNAL(sig2(int)), foo, SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig2()), SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig2(int)), SLOT(deleteLater()));
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
     }
     using namespace Namespace;
 }
-
+namespace UsingNested {
+    using namespace Namespace::Nested;
+    void test() {
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
+    }
+}
+namespace UsingNamespaceAndNested {
+    using namespace Namespace::Nested;
+    using namespace Namespace;
+    void test() {
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
+    }
+}
 namespace UsingFoo {
     using Namespace::Foo;
     void test() {
-        QObject::connect(foo, SIGNAL(sig1()), foo, SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig1()), SLOT(deleteLater()));
-        QObject::connect(foo, SIGNAL(sig2()), foo, SLOT(deleteLater()));
-        QObject::connect(foo, SIGNAL(sig2(int)), foo, SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig2()), SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig2(int)), SLOT(deleteLater()));
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
+    }
+}
+namespace UsingBar {
+    using Namespace::Nested::Bar;
+    void test() {
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
     }
 }
 namespace UsingFooAfterFunc {
     // using directive is after function -> have to qualify
     void test() {
-        QObject::connect(foo, SIGNAL(sig1()), foo, SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig1()), SLOT(deleteLater()));
-        QObject::connect(foo, SIGNAL(sig2()), foo, SLOT(deleteLater()));
-        QObject::connect(foo, SIGNAL(sig2(int)), foo, SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig2()), SLOT(deleteLater()));
-        foo->connect(foo, SIGNAL(sig2(int)), SLOT(deleteLater()));
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
     }
     using Namespace::Foo;
+}
+namespace UsingBarAfterFunc {
+    // using directive is after function -> have to qualify
+    void test() {
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
+    }
+    using Namespace::Nested::Bar;
+}
+namespace UsingFooAndBar {
+    using Namespace::Foo;
+    using Namespace::Nested::Bar;
+    void test() {
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
+    }
+}
+namespace UsingFooAndBarAfterFunc {
+    // using directive is after function -> have to qualify
+    void test() {
+        QObject::connect(foo, SIGNAL(sig(int)),
+                foo, SLOT(deleteLater()));
+        bar->connect(foo, SIGNAL(sig()),
+                SLOT(deleteLater()));
+        QObject::connect(bar, SIGNAL(sig()),
+                bar, SLOT(deleteLater()));
+        foo->connect(bar, SIGNAL(sig(int)),
+                SLOT(deleteLater()));
+    }
+    using Namespace::Foo;
+    using Namespace::Nested::Bar;
 }
 
 int main() {
--- tests/refactored/using_directives.cpp
+++ tests/refactored/using_directives.cpp
@@ -6,61 +6,161 @@ class Foo : public QObject {
 public:
     Foo() = default;
 Q_SIGNALS:
-    void sig1();
-    void sig2();
-    void sig2(int);
+    void sig();
+    void sig(int);
 };
+
+namespace Nested {
+class Bar : public QObject {
+    Q_OBJECT
+public:
+    Bar() = default;
+Q_SIGNALS:
+    void sig();
+    void sig(int);
+};
+}
 }
 
 static Namespace::Foo* foo = new Namespace::Foo();
+static Namespace::Nested::Bar* bar = new Namespace::Nested::Bar();
 
 namespace UsingNamespace {
     using namespace Namespace;
     void test() {
-        QObject::connect(foo, &Foo::sig1, foo, &Foo::deleteLater);
-        QObject::connect(foo, &Foo::sig1, foo, &Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Foo::*)()>(&Foo::sig2), foo, &Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Foo::*)(int)>(&Foo::sig2), foo, &Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Foo::*)()>(&Foo::sig2), foo, &Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Foo::*)(int)>(&Foo::sig2), foo, &Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Foo::*)(int)>(&Foo::sig),
+                foo, &Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Foo::*)()>(&Foo::sig),
+                bar, &Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Nested::Bar::*)()>(&Nested::Bar::sig),
+                bar, &Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Nested::Bar::*)(int)>(&Nested::Bar::sig),
+                foo, &Foo::deleteLater);
     }
 }
-
 namespace UsingNamespaceAfterFunc {
     // using directive is after function -> have to qualify
     void test() {
-        QObject::connect(foo, &Namespace::Foo::sig1, foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, &Namespace::Foo::sig1, foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig2), foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig2), foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig2), foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig2), foo, &Namespace::Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig),
+                foo, &Namespace::Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)()>(&Namespace::Nested::Bar::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)(int)>(&Namespace::Nested::Bar::sig),
+                foo, &Namespace::Foo::deleteLater);
     }
     using namespace Namespace;
 }
-
+namespace UsingNested {
+    using namespace Namespace::Nested;
+    void test() {
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig),
+                foo, &Namespace::Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig),
+                bar, &Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Bar::*)()>(&Bar::sig),
+                bar, &Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Bar::*)(int)>(&Bar::sig),
+                foo, &Namespace::Foo::deleteLater);
+    }
+}
+namespace UsingNamespaceAndNested {
+    using namespace Namespace::Nested;
+    using namespace Namespace;
+    void test() {
+        QObject::connect(foo, static_cast<void(Foo::*)(int)>(&Foo::sig),
+                foo, &Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Foo::*)()>(&Foo::sig),
+                bar, &Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Bar::*)()>(&Bar::sig),
+                bar, &Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Bar::*)(int)>(&Bar::sig),
+                foo, &Foo::deleteLater);
+    }
+}
 namespace UsingFoo {
     using Namespace::Foo;
     void test() {
-        QObject::connect(foo, &Foo::sig1, foo, &Foo::deleteLater);
-        QObject::connect(foo, &Foo::sig1, foo, &Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Foo::*)()>(&Foo::sig2), foo, &Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Foo::*)(int)>(&Foo::sig2), foo, &Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Foo::*)()>(&Foo::sig2), foo, &Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Foo::*)(int)>(&Foo::sig2), foo, &Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Foo::*)(int)>(&Foo::sig),
+                foo, &Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Foo::*)()>(&Foo::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)()>(&Namespace::Nested::Bar::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)(int)>(&Namespace::Nested::Bar::sig),
+                foo, &Foo::deleteLater);
+    }
+}
+namespace UsingBar {
+    using Namespace::Nested::Bar;
+    void test() {
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig),
+                foo, &Namespace::Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig),
+                bar, &Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Bar::*)()>(&Bar::sig),
+                bar, &Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Bar::*)(int)>(&Bar::sig),
+                foo, &Namespace::Foo::deleteLater);
     }
 }
 namespace UsingFooAfterFunc {
     // using directive is after function -> have to qualify
     void test() {
-        QObject::connect(foo, &Namespace::Foo::sig1, foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, &Namespace::Foo::sig1, foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig2), foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig2), foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig2), foo, &Namespace::Foo::deleteLater);
-        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig2), foo, &Namespace::Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig),
+                foo, &Namespace::Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)()>(&Namespace::Nested::Bar::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)(int)>(&Namespace::Nested::Bar::sig),
+                foo, &Namespace::Foo::deleteLater);
     }
     using Namespace::Foo;
+}
+namespace UsingBarAfterFunc {
+    // using directive is after function -> have to qualify
+    void test() {
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig),
+                foo, &Namespace::Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)()>(&Namespace::Nested::Bar::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)(int)>(&Namespace::Nested::Bar::sig),
+                foo, &Namespace::Foo::deleteLater);
+    }
+    using Namespace::Nested::Bar;
+}
+namespace UsingFooAndBar {
+    using Namespace::Foo;
+    using Namespace::Nested::Bar;
+    void test() {
+        QObject::connect(foo, static_cast<void(Foo::*)(int)>(&Foo::sig),
+                foo, &Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Foo::*)()>(&Foo::sig),
+                bar, &Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Bar::*)()>(&Bar::sig),
+                bar, &Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Bar::*)(int)>(&Bar::sig),
+                foo, &Foo::deleteLater);
+    }
+}
+namespace UsingFooAndBarAfterFunc {
+    // using directive is after function -> have to qualify
+    void test() {
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)(int)>(&Namespace::Foo::sig),
+                foo, &Namespace::Foo::deleteLater);
+        QObject::connect(foo, static_cast<void(Namespace::Foo::*)()>(&Namespace::Foo::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)()>(&Namespace::Nested::Bar::sig),
+                bar, &Namespace::Nested::Bar::deleteLater);
+        QObject::connect(bar, static_cast<void(Namespace::Nested::Bar::*)(int)>(&Namespace::Nested::Bar::sig),
+                foo, &Namespace::Foo::deleteLater);
+    }
+    using Namespace::Foo;
+    using Namespace::Nested::Bar;
 }
 
 int main() {
